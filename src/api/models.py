0e562442d05a7c99d7ba9ab45c244ef47923cbfa
ChrisKeith01 <165314148+ChrisKeith01@users.noreply.github.com>
2025-03-28 16:05:32 +0000

    ProjectManement

--- src/api/models.py
+++ src/api/models.py
@@ -4,13 +4,74 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Freelancer(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
 
+class Client(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+class Project_Manger(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+class Project(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    project_manager: Mapped[str] = mapped_column(String(120),nullable=False)
+    client: Mapped[str] = mapped_column(String(120), nullable=False)
+    budget: Mapped[float] = mapped_column(nullable=False)
+    freelancers: Mapped[str] = mapped_column(String(120),nullable=False)
+    tasks: Mapped[str] = mapped_column(String(120),nullable=False)
+    
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+           
+        }
+        
+
+class Task(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120),nullable=False)
+    description: Mapped[str] = mapped_column(String(120),nullable=False)
+    progress: Mapped[str] = mapped_column(String(120),nullable=False)
+    project_id: Mapped[str] = mapped_column(String(120),nullable=False)
+    assignee: Mapped[str] = mapped_column(String(120),nullable=False)
+    due_date: Mapped[str] = mapped_column(String(120),nullable=False)
+    cost: Mapped[str] = mapped_column(String(120),nullable=False)
+    
+    
     def serialize(self):
         return {
             "id": self.id,
